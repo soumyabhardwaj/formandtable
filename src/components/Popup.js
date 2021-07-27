4238b6a8dcd479c633c4b2e9f6ae66d803d7e3c4
soumyabhardwaj <63638319+soumyabhardwaj@users.noreply.github.com>
2021-07-27 14:37:59 +0530

    Update Popup.js

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -5,9 +5,9 @@ import CloseIcon from '@material-ui/icons/Close';
 
 const useStyles = makeStyles(theme => ({
     dialogWrapper: {
-        padding: theme.spacing(2),
+        padding: theme.spacing(0.5),
         position: 'absolute',
-        top: theme.spacing(5)
+        top: theme.spacing(2)
     },
     dialogTitle: {
         paddingRight: '0px'
